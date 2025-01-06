78fe5a234d87ce1ad86ad87bdbcd1a72f5eb4cd4
jmc <33655003+jmcook1186@users.noreply.github.com>
2025-01-06 15:18:31 +0000

    fic mass loss calculation

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from util import *
 from inputs import inputs
 
 def run_model():
-    for i in range(0,10,1):
-        m = calculate_mass_loss_per_timestep(inputs)
-        print(m)
+    for i in range(0,1,1):
+        m, ma, mi = calculate_mass_loss_per_timestep(inputs)
+    
+    print(m, "\n", mi, "\n", ma)
     return
 
 run_model()
--- util.py
+++ util.py
@@ -1,24 +1,39 @@
-
-
 def calculate_mass_loss_per_timestep(inputs):
 
     # set initial conditions
     n_layers = len(inputs["layer_thicknesses"])
     ks = k_in_subsurface_layers(inputs["k_star"], n_layers, inputs["layer_thicknesses"], inputs["extinction_coefficient"], inputs["delta_t"])
     qma = energy_at_surface(inputs["l_star"], inputs["qh"], inputs["qe"])
+    masses = calculate_mass_per_layer(inputs["densities"], inputs["layer_thicknesses"])
     # set up output array
-    ms = []
+    total_mass_loss = []
+    mass_loss_ablation = []
+    mass_loss_internal = []
 
     for n in range(0,len(ks), 1):
+
         qmi = ks[n]
         p_ice = inputs["densities"][n]
         ma, mi, m = mass_loss_in_subsurface_layers(qma, qmi, p_ice, inputs["lf"])
-        ms.append(m)
+        
+        if n==0:
+            print("here", m, mi, ma)
+            total_mass_loss.append(m)
+            mass_loss_ablation.append(ma)
+            mass_loss_internal.append(mi)        
+        else:
+            total_mass_loss.append(mi)
+            mass_loss_ablation.append(0)
+            mass_loss_internal.append(mi)
 
-    print("melt mass per layer (kg): ", ms)
-    
-    return m
+    return total_mass_loss, mass_loss_ablation, mass_loss_internal
 
+
+def calculate_mass_per_layer(densities, layer_thicknesses):
+    masses = []
+    for i in range(0, len(layer_thicknesses), 1):
+        masses.append(densities[i] * layer_thicknesses[i])
+    return masses
 
 def k_in_subsurface_layers(k_star, n_layers, layer_thicknesses, extinction_coefficient, delta_t):
     """K* value for each layer in column using Beer's law
@@ -38,8 +53,10 @@ def k_in_subsurface_layers(k_star, n_layers, layer_thicknesses, extinction_coeff
     for n in range(0, n_layers, 1):
         zl = sum(layer_thicknesses[0:n+1])
         zu = zl-layer_thicknesses[n]
-        k_layer = (k_star**(extinction_coefficient*zu) - k_star**(extinction_coefficient*zl)) * delta_t
+        k_layer = ((k_star**(-extinction_coefficient*zu)) - (k_star**(-extinction_coefficient*zl))) * delta_t
         ks.append(k_layer)
+
+    #print("Ks: \n", ks)
     return ks
 
 
@@ -75,5 +92,23 @@ def mass_loss_in_subsurface_layers(qma, qmi, p_ice, lf):
     
     mi = qmi / (p_ice * lf)
     ma = qma /(p_ice * lf)
-    m = ma + mi
+    m = mi + ma
+    # add ablation mass loss to upper layer only
     return ma, mi, m
+
+
+def calculate_densities_in_each_layer(masses, total_mass_loss, mass_loss_internal):
+    
+    densities = (masses - total_mass_loss) / ()
+
+
+    return densities
+
+
+# def calculate_volume_lost_to_ablation(mass_loss_ablation, p_ice):
+
+#     vas = []
+#     for i in range(0, len(mass_loss_ablation), 1):
+#         va = mass_loss_ablation[i] / p_ice[i]
+#         vas.append(va)
+#     return vas
